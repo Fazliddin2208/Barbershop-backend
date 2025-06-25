a7d60155d02c7bf14c13e491bbff1db2ba71ec4e
Fazliddin2208 <fazliddinabdurrahmon@gmail.com>
2025-06-25 17:13:58 +0500

    booking

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { BookingService } from './booking.service';
+import { Prisma } from '@prisma/client';
 
-@Controller('booking')
-export class BookingController {}
+@Controller('bookings')
+export class BookingController {
+  constructor(private readonly bookingService: BookingService) {}
+
+  @Post()
+  create(@Body() data: Prisma.BookingCreateInput) {
+    return this.bookingService.create(data);
+  }
+
+  @Get()
+  findAll() {
+    return this.bookingService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.bookingService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() data: Prisma.BookingUpdateInput) {
+    return this.bookingService.update(id, data);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.bookingService.remove(id);
+  }
+}
--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -1,4 +1,64 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
-export class BookingService {}
+export class BookingService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: Prisma.BookingCreateInput) {
+    return this.prisma.$transaction(async (tx) => {
+      // 1. Booking yaratish
+      const booking = await tx.booking.create({
+        data,
+        include: {
+          user: true,
+          timeslot: true,
+          service: true,
+        },
+      });
+
+      // 2. Tegishli TimeSlot isBooked ni true qilish
+      await tx.timeSlot.update({
+        where: { id: booking.timeSlotId },
+        data: { isBooked: true },
+      });
+
+      return booking;
+    });
+  }
+
+  findAll() {
+    return this.prisma.booking.findMany({
+      include: {
+        user: true,
+        timeslot: true,
+        service: true,
+      },
+    });
+  }
+
+  findOne(id: string) {
+    return this.prisma.booking.findUnique({
+      where: { id },
+      include: {
+        user: true,
+        timeslot: true,
+        service: true,
+      },
+    });
+  }
+
+  update(id: string, data: Prisma.BookingUpdateInput) {
+    return this.prisma.booking.update({
+      where: { id },
+      data,
+    });
+  }
+
+  remove(id: string) {
+    return this.prisma.booking.delete({
+      where: { id },
+    });
+  }
+}
